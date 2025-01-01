b308771648125a03c15d9fbbe1cd8af09dd19c12
Ahmed Shakib Reza <118752892+shakib003@users.noreply.github.com>
2025-01-02 02:52:28 +0600

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,4 +1,5 @@
 # Tableas for database
+##
 from . import db
 from flask_login import UserMixin
 from datetime import datetime
@@ -28,3 +29,48 @@ class Customer(db.Model, UserMixin):
 
     def __str__(self):
         return '<Customer %r>' % Customer.id
+    
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(100), nullable=False)
+    current_price = db.Column(db.Float, nullable=False)
+    previous_price = db.Column(db.Float, nullable=False)
+    in_stock = db.Column(db.Integer, nullable=False)
+    product_picture = db.Column(db.String(1000), nullable=False)
+    flash_sale = db.Column(db.Boolean, default=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    carts = db.relationship('Cart', backref=db.backref('product', lazy=True))
+    orders = db.relationship('Order', backref=db.backref('product', lazy=True))
+
+    def __str__(self):
+        return '<Product %r>' % self.product_name
+    
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    customer_link = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    product_link = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+
+    # customer product
+
+    def __str__(self):
+        return '<Cart %r>' % self.id
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(100), nullable=False)
+    payment_id = db.Column(db.String(1000), nullable=False)
+
+    customer_link = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    product_link = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+
+    # customer
+
+    def __str__(self):
+        return '<Order %r>' % self.id
