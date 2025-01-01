dee63bfda93a2338605eea31de950550edde1c10
Ahmed Shakib Reza <118752892+shakib003@users.noreply.github.com>
2025-01-02 02:52:40 +0600

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,99 +1,100 @@
-# ULR endpoints and routes
+#ULR endpoints and routes
+##
 
-#from flask import Blueprint, render_template
-
-#views = Blueprint('views', __name__)
-
-#@views.route('/')
-#def home():
-    #return render_template('base.html')
-
-from flask import Blueprint, render_template, flash, redirect, request, jsonify
-from .models import Product, Cart, Order
-from flask_login import login_required, current_user
-from . import db
-from intasend import APIService
-
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
-API_PUBLISHABLE_KEY = 'YOUR_PUBLISHABLE_KEY'
-
-API_TOKEN = 'YOUR_API_TOKEN'
-
-
 @views.route('/')
 def home():
+    return render_template('home.html')
 
-    items = Product.query.filter_by(flash_sale=True)
-
-    return render_template('home.html', items=items, cart=Cart.query.filter_by(customer_link=current_user.id).all()
-                           if current_user.is_authenticated else [])
-
-
-@views.route('/add-to-cart/<int:item_id>')
-@login_required
-def add_to_cart(item_id):
-    item_to_add = Product.query.get(item_id)
-    item_exists = Cart.query.filter_by(product_link=item_id, customer_link=current_user.id).first()
-    if item_exists:
-        try:
-            item_exists.quantity = item_exists.quantity + 1
-            db.session.commit()
-            flash(f' Quantity of { item_exists.product.product_name } has been updated')
-            return redirect(request.referrer)
-        except Exception as e:
-            print('Quantity not Updated', e)
-            flash(f'Quantity of { item_exists.product.product_name } not updated')
-            return redirect(request.referrer)
-
-    new_cart_item = Cart()
-    new_cart_item.quantity = 1
-    new_cart_item.product_link = item_to_add.id
-    new_cart_item.customer_link = current_user.id
-
-    try:
-        db.session.add(new_cart_item)
-        db.session.commit()
-        flash(f'{new_cart_item.product.product_name} added to cart')
-    except Exception as e:
-        print('Item not added to cart', e)
-        flash(f'{new_cart_item.product.product_name} has not been added to cart')
-
-    return redirect(request.referrer)
+# from flask import Blueprint, render_template, flash, redirect, request, jsonify
+# from .models import Product, Cart, Order
+# from flask_login import login_required, current_user
+# from . import db
+# from intasend import APIService
 
 
-@views.route('/cart')
-@login_required
-def show_cart():
-    cart = Cart.query.filter_by(customer_link=current_user.id).all()
-    amount = 0
-    for item in cart:
-        amount += item.product.current_price * item.quantity
+# views = Blueprint('views', __name__)
 
-    return render_template('cart.html', cart=cart, amount=amount, total=amount+200)
+# API_PUBLISHABLE_KEY = 'YOUR_PUBLISHABLE_KEY'
+
+# API_TOKEN = 'YOUR_API_TOKEN'
 
 
-@views.route('/pluscart')
-@login_required
-def plus_cart():
-    if request.method == 'GET':
-        cart_id = request.args.get('cart_id')
-        cart_item = Cart.query.get(cart_id)
-        cart_item.quantity = cart_item.quantity + 1
-        db.session.commit()
+# @views.route('/')
+# def home():
 
-        cart = Cart.query.filter_by(customer_link=current_user.id).all()
+#     items = Product.query.filter_by(flash_sale=True)
 
-        amount = 0
+#     return render_template('home.html', items=items, cart=Cart.query.filter_by(customer_link=current_user.id).all()
+#                            if current_user.is_authenticated else [])
 
-        for item in cart:
-            amount += item.product.current_price * item.quantity
 
-        data = {
-            'quantity': cart_item.quantity,
-            'amount': amount,
-            'total': amount + 200
-        }
+# @views.route('/add-to-cart/<int:item_id>')
+# @login_required
+# def add_to_cart(item_id):
+#     item_to_add = Product.query.get(item_id)
+#     item_exists = Cart.query.filter_by(product_link=item_id, customer_link=current_user.id).first()
+#     if item_exists:
+#         try:
+#             item_exists.quantity = item_exists.quantity + 1
+#             db.session.commit()
+#             flash(f' Quantity of { item_exists.product.product_name } has been updated')
+#             return redirect(request.referrer)
+#         except Exception as e:
+#             print('Quantity not Updated', e)
+#             flash(f'Quantity of { item_exists.product.product_name } not updated')
+#             return redirect(request.referrer)
 
-        return jsonify(data)
+#     new_cart_item = Cart()
+#     new_cart_item.quantity = 1
+#     new_cart_item.product_link = item_to_add.id
+#     new_cart_item.customer_link = current_user.id
+
+#     try:
+#         db.session.add(new_cart_item)
+#         db.session.commit()
+#         flash(f'{new_cart_item.product.product_name} added to cart')
+#     except Exception as e:
+#         print('Item not added to cart', e)
+#         flash(f'{new_cart_item.product.product_name} has not been added to cart')
+
+#     return redirect(request.referrer)
+
+
+# @views.route('/cart')
+# @login_required
+# def show_cart():
+#     cart = Cart.query.filter_by(customer_link=current_user.id).all()
+#     amount = 0
+#     for item in cart:
+#         amount += item.product.current_price * item.quantity
+
+#     return render_template('cart.html', cart=cart, amount=amount, total=amount+200)
+
+
+# @views.route('/pluscart')
+# @login_required
+# def plus_cart():
+#     if request.method == 'GET':
+#         cart_id = request.args.get('cart_id')
+#         cart_item = Cart.query.get(cart_id)
+#         cart_item.quantity = cart_item.quantity + 1
+#         db.session.commit()
+
+#         cart = Cart.query.filter_by(customer_link=current_user.id).all()
+
+#         amount = 0
+
+#         for item in cart:
+#             amount += item.product.current_price * item.quantity
+
+#         data = {
+#             'quantity': cart_item.quantity,
+#             'amount': amount,
+#             'total': amount + 200
+#         }
+
+#         return jsonify(data)
