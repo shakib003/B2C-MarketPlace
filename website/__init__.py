ad9a89d4af73751ad9b0de4d754a394e0522a179
Ahmed Shakib Reza <118752892+shakib003@users.noreply.github.com>
2025-01-02 16:41:52 +0600

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -19,9 +19,9 @@ def create_app():
 
     db.init_app(app)
 
-    # @app.errorhandler(404)
-    # def page_not_found(error):
-    #     return render_template('404.html')
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('404.html')
 
     login_manager = LoginManager()
     login_manager.init_app(app)
