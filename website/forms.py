2cb0015b1eff20dde4f2d66e6a24a798a584ad40
Ahmed Shakib Reza <118752892+shakib003@users.noreply.github.com>
2025-01-02 16:41:36 +0600

    Update forms.py

--- website/forms.py
+++ website/forms.py
@@ -25,3 +25,23 @@ class PasswordChangeForm(FlaskForm):
     new_password = PasswordField('New Password', validators=[DataRequired(), length(min=6)])
     confirm_new_password = PasswordField('Confirm New Password', validators=[DataRequired(), length(min=6)])
     change_password = SubmitField('Change Password')
+
+
+class ShopItemsForm(FlaskForm):
+    product_name = StringField('Name of Product', validators=[DataRequired()])
+    current_price = FloatField('Current Price', validators=[DataRequired()])
+    previous_price = FloatField('Previous Price', validators=[DataRequired()])
+    in_stock = IntegerField('In Stock', validators=[DataRequired(), NumberRange(min=0)])
+    product_picture = FileField('Product Picture', validators=[DataRequired()])
+    flash_sale = BooleanField('Flash Sale')
+
+    add_product = SubmitField('Add Product')
+    update_product = SubmitField('Update')
+
+
+class OrderForm(FlaskForm):
+    order_status = SelectField('Order Status', choices=[('Pending', 'Pending'), ('Accepted', 'Accepted'),
+                                                        ('Out for delivery', 'Out for delivery'),
+                                                        ('Delivered', 'Delivered'), ('Canceled', 'Canceled')])
+
+    update = SubmitField('Update Status')
